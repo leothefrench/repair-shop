89246ca0c8298796abec477d6266d36161451f72
Léandro DA SILVA <leothefrench@gmail.com>
2024-10-17 16:05:53 +0200

    implementation Header component with NavButton inside

---
+++ src/components/ui/NavButton.tsx
@@ -0,0 +1,32 @@
+import { LucideIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from 'next/link'
+
+type NavButtonProps = {
+    icon: LucideIcon,
+    label: string,
+    href?: string,
+}
+
+export const NavButton = ({ icon: Icon, label, href }: NavButtonProps) => {
+  return (
+    <Button
+        variant='ghost'
+        size='icon'
+        aria-label={label}
+        title={label}
+        className='rounded-full'
+        asChild
+    >
+        {
+            href ? (
+                <Link href={href}>
+                    <Icon />
+                </Link>
+            ) : (
+                <Icon />
+            )
+        }
+    </Button>
+  )
+};
